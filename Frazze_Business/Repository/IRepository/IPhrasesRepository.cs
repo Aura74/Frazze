19ed17a5eeccbd48f696b57c3c5783149f582f8a
Lars Asplund <lars.asplund@hotmail.com>
2022-10-04 14:32:21 +0200

    tisem

---
+++ Frazze_Business/Repository/IRepository/IPhrasesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazze_Business.Repository.IRepository
+{
+    public interface IPhrasesRepository
+    {
+    }
+}
---
+++ Frazze_DataAccess/Migrations/20221004120620_nrone.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Frazze_DataAccess.Migrations
+{
+    public partial class nrone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Phrases",
+                columns: table => new
+                {
+                    PhraseID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Phrase = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Culture = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrginalPhrase = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhraseDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Element = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phrases", x => x.PhraseID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Phrases");
+        }
+    }
+}
