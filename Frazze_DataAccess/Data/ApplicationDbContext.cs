20d3f138725b3c6dcc3c96d688366541e273c39a
Lars Asplund <lars.asplund@hotmail.com>
2022-10-04 14:05:11 +0200

    Add project files.

---
+++ Frazze_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Frazze_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Phrases> Phrases { get; set; }
+    }
+}
---
+++ Frazze_DataAccess/Phrases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazze_DataAccess
+{
+    public class Phrases
+    {
+        [Key]
+        public int PhraseID { get; set; }
+        public string Phrase { get; set; }
+        public string Culture { get; set; }
+        public string? OrginalPhrase { get; set; }
+        public string? PhraseDescription { get; set; }
+        public string Element { get; set; }
+    }
+}
