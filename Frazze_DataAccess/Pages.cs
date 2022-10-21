2fc2a4d1597c4aae03b6d8ca7d6394a626fb80e5
Lars <lars.asplund@hotmail.com>
2022-10-21 10:27:59 +0200

    innan

---
+++ Frazze_DataAccess/Pages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazze_DataAccess
+{
+    public class Pages
+    {
+        public int ID { get; set; }
+        public string PageName { get; set; }
+        public int ApplicationID { get; set; }
+        public DateTime created { get; set; }
+
+    }
+}
